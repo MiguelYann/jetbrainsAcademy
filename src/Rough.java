9dd617a0658504a1d7467b2b34732770a5beebd8
Miguel Yann <tsambamiguelito@gmail.com>
2020-09-08 21:23:34 +0200

    Edit gitignore

--- src/Rough.java
+++ src/Rough.java
@@ -1,16 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
-
 public class Rough {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int firstNumber = scanner.nextInt();
-
-        int second = scanner.nextInt();
-        System.out.println(firstNumber);
-        System.out.println(second);
-
     }
 }
